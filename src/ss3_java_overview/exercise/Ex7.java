75d94a4bb60967b1ce8cfce8e3ea66b64b7e3def
hi-im-na <lengocanh2k@gmail.com>
2024-12-12 21:02:36 +0700

    Ex7 ver 2

--- src/ss3_java_overview/exercise/Ex7.java
+++ src/ss3_java_overview/exercise/Ex7.java
@@ -1,56 +1,37 @@
 package ss3_java_overview.exercise;
 
 public class Ex7 {
-    public static void cauA(int i, int j) {
-        int a = i++ + j++;
+    public static void calc(char chr) {
+        int a, i = 1, j = 1;
+        switch (chr) {
+            case 'a' -> a = i++ + j++;
+            case 'b' -> a = i++ + ++j;
+            case 'c' -> a = ++i + j++;
+            case 'd' -> a = ++i + ++j;
+            case 'e' -> a = i++ + j++ + i++ + j++;
+            case 'f' -> a = ++i + ++j + i++ + j++;
+            case 'g' -> {
+                a = i++ + ++i - i-- - --i;
+                System.out.printf("a = %d, i = %d\n", a, i);
+                return;
+            }
+            case 'h' -> {
+                a = 10;
+                a += a++ + ++a;
+                System.out.printf("a = %d\n", a);
+                return;
+            }
+            default -> {
+                return;
+            }
+        }
         System.out.printf("a = %d, i = %d, j = %d\n", a, i, j);
     }
 
-    public static void cauB(int i, int j) {
-        int a = i++ + ++j;
-        System.out.printf("a = %d, i = %d, j = %d\n", a, i, j);
-    }
-
-    public static void cauC(int i, int j) {
-        int a = ++i + j++;
-        System.out.printf("a = %d, i = %d, j = %d\n", a, i, j);
-    }
-
-    public static void cauD(int i, int j) {
-        int a = ++i + ++j;
-        System.out.printf("a = %d, i = %d, j = %d\n", a, i, j);
-    }
-
-    public static void cauE(int i, int j) {
-        int a = i++ + j++ + i++ + j++;
-        System.out.printf("a = %d, i = %d, j = %d\n", a, i, j);
-    }
-
-    public static void cauF(int i, int j) {
-        int a = ++i + ++j + i++ + j++;
-        System.out.printf("a = %d, i = %d, j = %d\n", a, i, j);
-    }
-
-    public static void cauG(int i) {
-        int a = i++ + ++i - i-- - --i;
-        System.out.printf("a = %d, i = %d\n", a, i);
-    }
-
-    public static void cauH() {
-        int a = 10;
-        a += a++ + ++a;
-        System.out.printf("a = %d\n", a);
-    }
 
     public static void main(String[] args) {
-        int i = 1, j = 1;
-        cauA(i, j);
-        cauB(i, j);
-        cauC(i, j);
-        cauD(i, j);
-        cauE(i, j);
-        cauF(i, j);
-        cauG(i);
-        cauH();
+        for (char chr = 'a'; chr <= 'h'; chr++) { // in ra ket qua tu cau a -> h
+            calc(chr);
+        }
     }
 }
