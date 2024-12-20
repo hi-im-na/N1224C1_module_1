ea2668e4c844d94c0ddfdfc16e42fac851cbf575
hi-im-na <lengocanh2k@gmail.com>
2024-12-20 08:45:56 +0700

    split code into functions

--- src/ss5_loop/exercise/Ex13.java
+++ src/ss5_loop/exercise/Ex13.java
@@ -4,6 +4,38 @@ import utils.CheckInputType;
 import utils.InputCommon;
 
 public class Ex13 {
+    public static int gcd(int a, int b) {
+        int greatestCommonDivisor = 1;
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+
+        // gcd
+        if (max % min == 0) {
+            greatestCommonDivisor = min;
+        } else {
+            for (int i = min / 2; i > 1; i--) {
+                if (a % i == 0 && b % i == 0) {
+                    greatestCommonDivisor = i;
+                }
+            }
+        }
+        return greatestCommonDivisor;
+    }
+
+    public static int lcm(int a, int b) {
+        int leastCommonMultiple = a * b;
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+
+        // lcm
+        for (int i = max; i <= a * b; i += max) {
+            if (i % min == 0) {
+                leastCommonMultiple = i;
+                break;
+            }
+        }
+        return leastCommonMultiple;
+    }
 
     public static void main(String[] args) {
         try (InputCommon ic = new InputCommon()) {
@@ -13,32 +45,10 @@ public class Ex13 {
             System.out.print("Nhap so nguyen duong b: ");
             int b = ic.checkInputInt(CheckInputType.MIN, 0);
 
-            int greatestCommonDivisor = 1, leastCommonMultiple = a * b;
-            int min = Math.min(a, b);
-            int max = Math.max(a, b);
-
-            // gcd
-            if (max % min == 0) {
-                greatestCommonDivisor = min;
-            } else {
-                for (int i = min / 2; i > 1; i--) {
-                    if (a % i == 0 && b % i == 0) {
-                        greatestCommonDivisor = i;
-                    }
-                }
-            }
-
-            // lcm
-            for (int i = max; i <= a * b; i += max) {
-                if (i % min == 0) {
-                    leastCommonMultiple = i;
-                    break;
-                }
-            }
 
             // result
-            System.out.println("Uoc chung lon nhat: " + greatestCommonDivisor);
-            System.out.println("Boi chung nho nhat: " + leastCommonMultiple);
+            System.out.println("Uoc chung lon nhat: " + gcd(a, b));
+            System.out.println("Boi chung nho nhat: " + lcm(a, b));
 
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
--- src/ss5_loop/exercise/Ex7.java
+++ src/ss5_loop/exercise/Ex7.java
@@ -4,20 +4,22 @@ import utils.CheckInputType;
 import utils.InputCommon;
 
 public class Ex7 {
+    public static void fibonacci(int n) {
+        int f1 = 0, f2 = 1, fn = 1;
+        System.out.print("Day Fibonacci: ");
+        while (fn <= n) {
+            System.out.print(fn + " ");
+            f1 = f2;
+            f2 = fn;
+            fn = f1 + f2;
+        }
+    }
+
     public static void main(String[] args) {
         try (InputCommon ic = new InputCommon()) {
             System.out.print("Nhap so n: ");
             int n = ic.checkInputInt(CheckInputType.MIN, 0);
-
-            int f1 = 0, f2 = 1, fn = 1;
-            System.out.print("Day Fibonacci: ");
-            while (fn <= n) {
-                System.out.print(fn + " ");
-                f1 = f2;
-                f2 = fn;
-                fn = f1 + f2;
-            }
-
+            fibonacci(n);
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
